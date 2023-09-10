a4fbbc35d5ac477090c60d42bf82e5ebcaaa3806
yanggggjie <60206592+yanggggjie@users.noreply.github.com>
2023-09-10 21:05:51 +0800

    Initial commit

---
+++ src/colors.ts
@@ -0,0 +1,48 @@
+export function index2bgColor(index: number) {
+  switch (index) {
+    case 0:
+      return 'bg-red-500'
+    case 1:
+      return 'bg-green-500'
+    case 2:
+      return 'bg-blue-500'
+    case 3:
+      return 'bg-yellow-500'
+    case 4:
+      return 'bg-pink-500'
+    case 5:
+      return 'bg-purple-500'
+  }
+}
+export function index2textColor(index: number) {
+  switch (index) {
+    case 0:
+      return 'text-red-500'
+    case 1:
+      return 'text-green-500'
+    case 2:
+      return 'text-blue-500'
+    case 3:
+      return 'text-yellow-500'
+    case 4:
+      return 'text-pink-500'
+    case 5:
+      return 'text-purple-500'
+  }
+}
+export function index2borderColor(index: number) {
+  switch (index) {
+    case 0:
+      return 'border-red-500'
+    case 1:
+      return 'border-green-500'
+    case 2:
+      return 'border-blue-500'
+    case 3:
+      return 'border-yellow-500'
+    case 4:
+      return 'border-pink-500'
+    case 5:
+      return 'border-purple-500'
+  }
+}
